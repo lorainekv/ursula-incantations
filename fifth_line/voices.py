9655e1f3eb8c58c5a11ef80253383fe3c95f3548
Loraine <loraine@lorainekv.com>
2016-10-29 21:08:46 -0700

    title method messes up with apostrophes.

---
+++ fifth_line/voices.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+voices = [
+"ahotsa",
+"Die Stimme",
+"dimits'i",
+"dzayny",
+"e le'o",
+"il-vuċi"
+"ka leo",
+"le leo",
+"na domo",
+"te reo",
+]
--- incant.py
+++ incant.py
@@ -1,14 +1,17 @@
+import string
 import random
+
 from first_line.beluga_sevruga import beluga_sevruga 
 from second_line.caspian_sea import caspian_sea
 from third_line.larynxes import larynxes 
 from third_line.glossitis import glossitis 
 from fourth_line.et import et
 from fourth_line.laryngitis import laryngitis
+from fifth_line.voices import voices
 
 # Beluga, Sevruga
 three_syllable_words = random.sample(beluga_sevruga, 2)
-three_syllable_words = [word.title() for word in three_syllable_words]
+three_syllable_words = [string.capwords(word) for word in three_syllable_words]
 print ", ".join(three_syllable_words)
 
 # Come winds of the Caspian Sea
@@ -16,7 +19,7 @@ sea = random.choice(caspian_sea)
 print "Come winds of the %s" % sea
 
 # Larynxes, glossitis
-xes_ces = random.choice(larynxes).title()
+xes_ces = string.capwords(random.choice(larynxes))
 itis = random.choice(glossitis)
 print "%s, %s" % (xes_ces, itis)
 
@@ -25,3 +28,6 @@ latin = random.choice(et)
 other_itis = random.choice(laryngitis)
 print "%s, %s" % (latin, other_itis)
 
+# La voce to me
+voice = string.capwords(random.choice(voices))
+print "%s to me" % voice
